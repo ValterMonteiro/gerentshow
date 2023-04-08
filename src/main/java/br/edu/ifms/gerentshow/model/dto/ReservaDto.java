cd94e9548d19d4285f8de6cea0032fd892fc58a7
ValterMonteiro <valtinms@gmail.com>
2023-04-08 16:21:49 -0400

    implementando e mogificando Reserva model dto e model mapper

--- src/main/java/br/edu/ifms/gerentshow/model/dto/ReservaDto.java
+++ src/main/java/br/edu/ifms/gerentshow/model/dto/ReservaDto.java
@@ -1,12 +1,23 @@
 package br.edu.ifms.gerentshow.model.dto;
 
 import java.sql.Date;
+import java.util.UUID;
 
 public class ReservaDto {
 
+	private UUID id;
+	
 	private Date data;
 	
 	private double valor;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
 
 	public Date getData() {
 		return data;
