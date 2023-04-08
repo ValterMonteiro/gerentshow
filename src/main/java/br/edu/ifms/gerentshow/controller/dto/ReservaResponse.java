c1e09bf215e8c2e14db1837da4b203fcad43687b
ValterMonteiro <valtinms@gmail.com>
2023-04-08 15:20:51 -0400

    implementando atributos e metodos Reserva

--- src/main/java/br/edu/ifms/gerentshow/controller/dto/ReservaResponse.java
+++ src/main/java/br/edu/ifms/gerentshow/controller/dto/ReservaResponse.java
@@ -1,5 +1,28 @@
 package br.edu.ifms.gerentshow.controller.dto;
 
+import java.sql.Date;
+
 public class ReservaResponse {
 
+	private Date data;
+	
+	private double valor;
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	
 }
