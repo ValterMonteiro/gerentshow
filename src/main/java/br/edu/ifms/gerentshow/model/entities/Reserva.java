f8d31be9964b32ecca5939d488f79b2e27f056b0
ValterMonteiro <valtinms@gmail.com>
2023-04-08 16:09:14 -0400

    criando e implementando Reserva entities

--- src/main/java/br/edu/ifms/gerentshow/model/entities/Reserva.java
+++ src/main/java/br/edu/ifms/gerentshow/model/entities/Reserva.java
@@ -1,5 +1,52 @@
 package br.edu.ifms.gerentshow.model.entities;
 
+import java.sql.Date;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_RESERVA")
 public class Reserva {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
+	
+	@Column(nullable = false, length = 8)
+	private Date data;
+	
+	@Column(nullable = false, length = 11)
+	private double valor;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	
 }
