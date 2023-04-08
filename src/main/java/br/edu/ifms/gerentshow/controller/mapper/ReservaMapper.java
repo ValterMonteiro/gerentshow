ec1982a54de2154da0210229f40ff942a99c4fa0
ValterMonteiro <valtinms@gmail.com>
2023-04-08 15:29:42 -0400

    implementando metodos ReservaMapper

--- src/main/java/br/edu/ifms/gerentshow/controller/mapper/ReservaMapper.java
+++ src/main/java/br/edu/ifms/gerentshow/controller/mapper/ReservaMapper.java
@@ -1,5 +1,22 @@
 package br.edu.ifms.gerentshow.controller.mapper;
 
+import br.edu.ifms.gerentshow.controller.dto.ReservaRequest;
+import br.edu.ifms.gerentshow.controller.dto.ReservaResponse;
+import br.edu.ifms.gerentshow.model.dto.ReservaDto;
+
 public class ReservaMapper {
 
+	public static ReservaDto requestToDto( ReservaRequest reservaRequest ) {
+		ReservaDto instrumentoDto = new ReservaDto();
+		ReservaDto.setData( reservaRequest.getData() );
+		ReservaDto.setValo( reservaRequest.getValor() );
+		return ReservaDto;
+	}
+	
+	public static ReservaResponse dtoToResponse(ReservaDto reservaDto ) {
+		ReservaResponse reservaResponse = new ReservaResponse();
+		reservaResponse.setData( reservaDto.getData() );
+		reservaResponse.setValor( reservaDto.getValor() );
+		return reservaResponse;
+	}
 }
